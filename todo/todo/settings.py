ced84261a3e5c63a9bcaadbbad3d99e988f38fd5
Suraj <suraj@quoqo.com>
2025-11-29 14:18:28 +0530

    sql_integrate commit

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import pymysql
+pymysql.install_as_MySQLdb()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,10 +84,18 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 # Using SQLite for development
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'todo',   # database name you created in MySQL
+        'USER': 'root',                 # your MySQL username
+        'PASSWORD': 'Suraj@123',    # your MySQL password
+        'HOST': 'localhost',            # database host
+        'PORT': '3306',                 # default MySQL port
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
+
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
